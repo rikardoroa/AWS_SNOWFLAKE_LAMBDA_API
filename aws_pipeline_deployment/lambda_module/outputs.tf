3ae3b9855271bf9bcc2518bf5c4b2674fc5987ad
Ricardo Roa <32416860+rikardoroa@users.noreply.github.com>
2025-05-07 14:38:16 -0500

    updating outputs

--- aws_pipeline_deployment/lambda_module/outputs.tf
+++ aws_pipeline_deployment/lambda_module/outputs.tf
@@ -1,11 +1,11 @@
 output "iam_for_dev_name" {
-  value = aws_iam_role.iam_for_dev.name
+  value = aws_iam_role.iam_dev_role.name
 }
 
 output "policy_for_dev" {
-  value = data.aws_iam_policy_document.pipeline_dev_policy.json
+  value = data.aws_iam_policy_document.pipeline_dev_policy_snowflake.json
 }
 
 output "iam_for_dev_arn" {
-  value = aws_iam_role.iam_for_dev.arn
+  value = aws_iam_role.iam_dev_role.arn
 }
