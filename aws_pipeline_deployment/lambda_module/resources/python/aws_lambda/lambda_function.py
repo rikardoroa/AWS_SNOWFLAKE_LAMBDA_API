255c7a7bffe965dd467fa8b0f291f9bed00d9026
Ricardo Roa <32416860+rikardoroa@users.noreply.github.com>
2025-05-07 23:13:20 -0500

    Update lambda_function.py

--- aws_pipeline_deployment/lambda_module/resources/python/aws_lambda/lambda_function.py
+++ aws_pipeline_deployment/lambda_module/resources/python/aws_lambda/lambda_function.py
@@ -6,28 +6,16 @@ def lambda_handler(event, context):
     
     api = SnowflakeApi()
     api.get_secret()
-    get  = GetSnowflakeData ()
+    get_data  = GetSnowflakeData ()
 
     method = event.get("httpMethod", "").upper()
 
     if method == "GET":
-        try:
+    
             employees = api.get_employee_data()
-            result = get.getdata(employees)
-            #print(employees)
-
-            # return {
-            #         "statusCode": 200,
-            #         "headers": {"Content-Type": "application/json"},
-            #         "body": json.dumps(employees, default=str)   
-            # }
-
-        except Exception as e:
-            return  {
-                    "statusCode": 500,
-                    "headers": {"Content-Type": "application/json"},
-                    "body": json.dumps({"error": str(e)}, default=str)  
-            }
+            result = get_data.getdata(employees)
+            return result
+         
 
     if method == "POST":
         payload = json.loads(event["body"])
